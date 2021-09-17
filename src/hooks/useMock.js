5644fd191fba61ee05980d3854da29c6ce7c1750
Ivan Espinola G <ivan.espinola@b2wdigital.com>
2021-09-17 20:21:47 -0300

    refactor: ajuste na logica erro backend

--- src/hooks/useMock.js
+++ src/hooks/useMock.js
@@ -2,23 +2,30 @@ import { useState, useEffect } from 'react';
 import { service } from '../service';
 
 const useMock = () => {
-  const [loading, setLoading] = useState(undefined);
-  const [error, setError] = useState(undefined);
-	const [data, setData] = useState(undefined);
+  const [loading, setLoading] = useState();
+  const [error, setError] = useState();
+	const [data, setData] = useState();
 	
 	const fetchService = () => {
-		service.getData(5000)
-			.then(res => setData(res.data))
+		service.getData()
+			.then(res => setData(res))
 			.catch(err => setError(err))
-      .finally(() => setLoading(false));
 	}
 	
+	useEffect(() => {
+		return data || error ? setLoading(false) : false;
+	}, [data, error]);
+
 	useEffect(() => {
 		return loading ? fetchService() : false;
 	}, [loading]);
 	
 	useEffect(() => {
 		setLoading(true);
+
+		return () => {
+			console.log('saiu')
+		};
   }, []);
 
   return {
--- src/service/mockError.js
+++ src/service/mockError.js
@@ -1,7 +1,17 @@
-const error = {
-  status: 503,
-  message: 'Service Unavailable',
-};
+const arrUsers = [
+  { prontuario: 458365, nome: 'Ana Luíza Fontes', email: 'ana.fontes@b2wdigital.com', bit: 'SP' },
+  { prontuario: 937247, nome: 'Cleonildo Guimarães', email: 'cleonildo.soares@b2wdigital.com', bit: 'RJ' },
+  { prontuario: 636552, nome: 'Ivan Espinola', email: 'ivan.espinola@b2wdigital.com', bit: 'SP' },
+  null,
+  { prontuario: 283654, nome: 'Gabriela Papp', email: 'gabi.papp@b2wdigital.com', bit: 'RJ' },
+  { prontuario: 378664, nome: 'Pedro Montes', email: 'pedro.montes@b2wdigital.com', bit: 'SP' },
+];
 
-export default error;
-export { error };
+const data = {
+	data: arrUsers,
+	status: 200,
+	message: 'ok',
+}
+
+export default data;
+export { data };

254771de1708ce33048a5f5096fa252f0107027f
Ana Luíza Fontes <ana.fontes@b2wdigital.com>
2021-09-21 16:19:21 -0300

    style: table column heading adjustment

--- __mock__/mockData.js
+++ __mock__/mockData.js
@@ -1,9 +1,9 @@
 const users = [
-  { prontuario: 458365, nome: 'Ana Luíza Fontes', email: 'ana.fontes@b2wdigital.com', bit: 'SP' },
-  { prontuario: 937247, nome: 'Cleonildo Guimarães', email: 'cleonildo.soares@b2wdigital.com', bit: 'RJ' },
-  { prontuario: 636552, nome: 'Ivan Espinola', email: 'ivan.espinola@b2wdigital.com', bit: 'SP' },
-  { prontuario: 283654, nome: 'Gabriela Papp', email: 'gabi.papp@b2wdigital.com', bit: 'RJ' },
-  { prontuario: 378664, nome: 'Pedro Montes', email: 'pedro.montes@b2wdigital.com', bit: 'SP' },
+  { prontuário: 458365, nome: 'Ana Luíza Fontes', email: 'ana.fontes@b2wdigital.com', bit: 'SP' },
+  { prontuário: 937247, nome: 'Cleonildo Guimarães', email: 'cleonildo.soares@b2wdigital.com', bit: 'RJ' },
+  { prontuário: 636552, nome: 'Ivan Espinola', email: 'ivan.espinola@b2wdigital.com', bit: 'SP' },
+  { prontuário: 283654, nome: 'Gabriela Papp', email: 'gabi.papp@b2wdigital.com', bit: 'RJ' },
+  { prontuário: 378664, nome: 'Pedro Montes', email: 'pedro.montes@b2wdigital.com', bit: 'SP' },
 ];
 
 export default users;

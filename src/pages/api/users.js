ba0142f0adc5f53c7648d606903972cddc2a4962
Ana Luíza Fontes <ana.fontes@b2wdigital.com>
2021-09-21 16:28:41 -0300

    feat: implementation of the function of setTimeout

--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -5,8 +5,10 @@ const randomBoolean = () => Math.random() < 0.5;
 const serviceUser = (req, res) => {
   const withDataError = randomBoolean();
   const corruptData = [...data, null];
-  return res.status(200).json(withDataError ? corruptData : data);
-}
+  setTimeout(() => {
+    res.status(200).json(withDataError ? corruptData : data);
+  }, 3000);
+};
 
 export default serviceUser;
 export { serviceUser };

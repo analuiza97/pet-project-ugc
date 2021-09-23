56e30f59b2b1aecf46a74fddba0fbf481bed440b
Ana Luíza Fontes <ana.fontes@b2wdigital.com>
2021-09-22 22:44:08 -0300

    feat: error boundary rendering component

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+
+const ErrorGif = styled.img`
+  max-width: 350px;
+`;
+
+const Warning = styled.img`
+  max-width: 35px;
+`;
+
+const ErrorMsg = styled.div`
+  overflow: hidden;
+  font-family: Quicksand, arial, sans-serif;
+  font-size: 20px;
+  padding: 5px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.05), 0 0px 40px rgba(0, 0, 0, 0.08);
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const RenderError = () => {
-  return <h1>Something went wrong</h1>;
+  return (
+    <Container>
+      <ErrorGif src="./images/error.gif" alt="error gif" />
+      <ErrorMsg>
+        <Warning src="./images/warning.png" alt="warning icon" />
+        Something went wrong! Please restart the page or try again later.
+      </ErrorMsg>
+    </Container>
+  );
 };
 
 export default class ErrorBoundary extends React.Component {
-  
   state = {
     hasError: false,
   };
@@ -25,7 +60,7 @@ export default class ErrorBoundary extends React.Component {
     if (!hasError) {
       return children;
     }
-    
+
     return <RenderError />;
   }
 }

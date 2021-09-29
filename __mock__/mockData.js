8672190dc64ae5a956fa74038ae369ae7feacb80
Ana Luíza Fontes <ana.fontes@b2wdigital.com>
2021-09-29 18:20:05 -0300

    chore: table adjustment

--- __mock__/mockData.js
+++ __mock__/mockData.js
@@ -4,6 +4,7 @@ const users = [
   { prontuario: 636552, nome: 'Ivan Espinola', email: 'ivan.espinola@b2wdigital.com', bit: 'SP' },
   { prontuario: 283654, nome: 'Gabriela Papp', email: 'gabi.papp@b2wdigital.com', bit: 'RJ' },
   { prontuario: 378664, nome: 'Pedro Montes', email: 'pedro.montes@b2wdigital.com', bit: 'SP' },
+  { prontuario: 963652, nome: 'Aline Marciano', email: 'aline.marciano@b2wdigital.com', bit: 'SP' },
 ];
 
 export default users;
